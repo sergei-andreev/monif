421204cc57919e52184bfca3f9f84556eb55ee06
Sergey Andreev <sergey.andreev@dualbootpartners.com>
2023-09-28 09:18:11 +0200

    bugfix: fix paths for bg images

--- src/blocks/modules/about-us/about-us.js
+++ src/blocks/modules/about-us/about-us.js
@@ -1,7 +1,16 @@
-const workers = ["Olivier", "Alena", "Tom", "Ivo"];
-const workersNames = ["Olivier Binet", "Alena Valovaya", "Thomas Finan", "Ivo Dimitrov"];
+import Swiper from "swiper";
+import { Pagination, Navigation } from "swiper/modules";
 
-var swiper = new Swiper(".swiper", {
+const workers = [
+  { name: "Olivier Binet", img: "Olivier" },
+  { name: "Alena Valovaya", img: "Alena" },
+  { name: "Thomas Finan", img: "Tom" },
+  { name: "Ivo Dimitrov", img: "Ivo" },
+];
+
+const swiper = new Swiper(".swiper", {
+  modules: [Pagination, Navigation],
+  spaceBetween: 100,
   pagination: {
     el: ".about-us__bullets",
     bulletActiveClass: "about-us__bullet_active",
@@ -9,9 +18,9 @@ var swiper = new Swiper(".swiper", {
     clickable: true,
     renderBullet: function (index, className) {
       return `<div class="${className}">
-      <img src="./img/workers/${workers[index]}.png" alt="" />
-      <p class="about-us__bullet-name">${workersNames[index]}</p>
-    </div>`;
+        <img src="./img/workers/${workers[index].img}.png" alt="" />
+        <p class="about-us__bullet-name">${workers[index].name}</p>
+      </div>`;
     },
   },
 });
